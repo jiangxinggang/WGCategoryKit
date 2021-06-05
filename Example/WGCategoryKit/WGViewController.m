993c1292852a51c62db1f6f06f45242422fbe105
jiang_xg_14@163.com <371326ab>
2021-06-05 19:24:43 +0800

    Initial commit

---
+++ Example/WGCategoryKit/WGViewController.m
@@ -0,0 +1,29 @@
+//
+//  WGViewController.m
+//  WGCategoryKit
+//
+//  Created by jiangxinggang on 06/05/2021.
+//  Copyright (c) 2021 jiangxinggang. All rights reserved.
+//
+
+#import "WGViewController.h"
+
+@interface WGViewController ()
+
+@end
+
+@implementation WGViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Example/WGCategoryKit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WGCategoryKit
+//
+//  Created by jiangxinggang on 06/05/2021.
+//  Copyright (c) 2021 jiangxinggang. All rights reserved.
+//
+
+@import UIKit;
+#import "WGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WGAppDelegate class]));
+    }
+}
